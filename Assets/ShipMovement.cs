b5b9711b6ed0b25de607969abff9b439c700fcd6
Josh Wehrly <josh.wehrly@gmail.com>
2014-01-25 10:25:18 -0800

    Configured Project for Version Control and added ship sprites.

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour
+{
+    private Vector3 windDirection = Vector3.down;
+
+	void Start () 
+    {
+	
+	}
+	
+    void Update ()
+    {
+        	
+	}
+
+    private void ReadInput()
+    {
+
+    }
+}
