d3be190ea821252b17afe98b0aa2a98b24affdb9
Josh Wehrly <josh.wehrly@gmail.com>
2014-01-26 15:31:34 -0800

    Added wind direction arrow.

---
+++ src/Assets/Scripts/Arrow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour
+{
+    private ShipMovement parent;
+
+	void Start ()
+    {
+        parent = transform.parent.gameObject.GetComponent<ShipMovement>();
+        gameObject.layer = LayerMask.NameToLayer("Player" + parent.joystickIndex.ToString());
+	}
+	
+	void Update ()
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector3.forward, parent.windDirectionWS);
+	}
+}
