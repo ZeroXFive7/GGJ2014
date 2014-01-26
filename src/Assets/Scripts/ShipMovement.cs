9f33fd19ac70e1054342f0ee6f3e368ba0751bf7
Josh Wehrly <josh.wehrly@gmail.com>
2014-01-25 17:27:18 -0800

    Added sail controls and rudder controls.

--- src/Assets/Scripts/ShipMovement.cs
+++ src/Assets/Scripts/ShipMovement.cs
@@ -9,15 +9,16 @@ public class ShipMovement : MonoBehaviour
 
     [SerializeField]
     private float MaxRudderAngle;
+
+    [SerializeField]
+    private float MaxSailAngle;
     
-    private Vector3 windDirection = new Vector3(-1.0f, 0.0f, 0.0f);
+    private Vector3 windDirectionWS = new Vector3(-1.0f, 0.0f, 0.0f);
 
-    private Vector3 sailDirection = new Vector3(0.0f, 0.0f, 1.0f);
-    private Vector3 rudderDirection = new Vector3(0.0f, 0.0f, -1.0f);
+    private Quaternion sailRotation = Quaternion.identity;
+    private Vector3 rudderTangentLS = new Vector3(0.0f, 0.0f, -1.0f);
 
-    private Vector3 localRudder = new Vector3(0.0f, 0.0f, -1.0f);
-
-    private float speed = 0.0f;
+    private float relativeSpeed = 0.0f;
 
     private enum Direction
     {
@@ -38,22 +39,24 @@ public class ShipMovement : MonoBehaviour
 	
     void Update ()
     {
+        Debug.DrawRay(transform.position + new Vector3(-5.0f, 0.0f, 5.0f), 5.0f * windDirectionWS, Color.magenta);
+
         ReadInput();
 
-        UpdateForward();
-        UpdateSpeed();
+        UpdateSail();
+        UpdateRudder();
 
-        rigidbody.velocity = -rudderDirection * speed;
+        rigidbody.velocity = transform.forward * MaxSpeed * relativeSpeed;
     }
 
-    void OnGui()
+    void OnGUI()
     {
-
+        
     }
 
     private void ReadInput()
     {
-        slack = -Input.GetAxis("Slack");
+        slack = Mathf.Max(Input.GetAxis("Slack"), 0.0f);
         rudder = Input.GetAxis("Rudder");
 
         foreach (Direction dir in Enum.GetValues(typeof(Direction)))
@@ -62,29 +65,53 @@ public class ShipMovement : MonoBehaviour
         }
     }
 
-    private void UpdateForward()
+    private void UpdateRudder()
     {
-        Quaternion rotation = Quaternion.Euler(0.0f, -rudder * MaxRudderAngle, 0.0f);
-        Vector3 targetLocal = (rotation * -Vector3.forward).normalized;
-        localRudder = Vector3.Lerp(localRudder, targetLocal, Time.deltaTime);
+        Quaternion rudderRotationLS = Quaternion.Euler(0.0f, -rudder * MaxRudderAngle, 0.0f);
+        Vector3 targetLocal = (rudderRotationLS * -Vector3.forward).normalized;
+        rudderTangentLS = Vector3.Lerp(rudderTangentLS, targetLocal, Time.deltaTime);
 
-        Vector3 targetRudderDirection = (Quaternion.FromToRotation(-Vector3.forward, localRudder) * rudderDirection).normalized;
-        rudderDirection = Vector3.Lerp(rudderDirection, targetRudderDirection, Time.deltaTime);
+        Vector3 targetRudderDirection = (Quaternion.FromToRotation(-Vector3.forward, rudderTangentLS) * -transform.forward).normalized;
+        Vector3 newForward = Vector3.Lerp(transform.forward, -targetRudderDirection, Time.deltaTime);
 
-        float angle = Vector3.Angle(Vector3.forward, -rudderDirection);
-        if (Vector3.Cross(Vector3.forward, -rudderDirection).y <= 0.0f)
-        {
-            angle *= -1.0f;
-        }
+        transform.rotation = Quaternion.FromToRotation(Vector3.forward, newForward);
 
-        transform.rotation = Quaternion.Euler(90.0f, angle, 0.0f);
-
-        Debug.DrawRay(transform.position, rudderDirection, Color.yellow);
-        Debug.DrawRay(transform.position, localRudder, Color.green);
+        Debug.DrawRay(transform.position - transform.forward * 2.0f, transform.rotation * rudderTangentLS, Color.red);
     }
 
-    private void UpdateSpeed()
+    private void UpdateSail()
     {
-        speed = (1.0f - Vector2.Dot(sailDirection, -windDirection)) * MaxSpeed;
+        UpdateSailRotation();
+        
+        Transform sail = transform.GetChild(0);
+        sail.transform.localRotation = sailRotation;
+
+        Vector3 sailNormalWS = sailRotation * transform.right;
+        if (Vector3.Cross(windDirectionWS, sailRotation * transform.forward).y < 0.0f)
+        {
+            sailNormalWS *= -1.0f;
+        }
+        Debug.DrawRay(transform.position, sailNormalWS, Color.blue);
+
+        relativeSpeed = Vector2.Dot(sailNormalWS, -windDirectionWS);
+    }
+
+    private void UpdateSailRotation()
+    {
+        Vector3 fullTaughtTangentWS = -transform.forward;
+        Vector3 fullSlackTangentWS = windDirectionWS;
+
+        Quaternion oldRotation = sailRotation;
+
+        Quaternion fullSlackRotation = Quaternion.FromToRotation(fullTaughtTangentWS, fullSlackTangentWS);
+        fullSlackRotation = Quaternion.Slerp(Quaternion.identity, fullSlackRotation, (1.0f - slack));
+
+        sailRotation = Quaternion.Lerp(sailRotation, fullSlackRotation, Time.deltaTime);
+        if (Vector3.Angle((sailRotation * fullTaughtTangentWS), fullTaughtTangentWS) > MaxSailAngle)
+        {
+            sailRotation = oldRotation;
+        }
+
+        Debug.DrawRay(transform.position, 2.5f * (sailRotation * fullTaughtTangentWS), Color.green);
     }
 }
